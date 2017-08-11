bacb07f0c64eaa122852e846ddb119164286e2ce
victor-iyiola <javafolabi@gmail.com>
2017-08-11 22:04:24 +0100

    Relative Layout. WhatsApp chat layout

---
+++ app/src/main/java/com/victoriyiola/udacitypractise/LayoutsRelative.java
@@ -0,0 +1,24 @@
+package com.victoriyiola.udacitypractise;
+
+/*
+ * @author "Victor I. Afolabi"
+ * Created by victor
+ * A.I. engineer & Software developer
+ * javafolabi@gmail.com
+ * On 11, Aug 2017 @ 9:33 PM
+ * Copyright victor © 2017. All rights reserved.
+ */
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LayoutsRelative extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layouts_relative);
+    }
+
+}
+
