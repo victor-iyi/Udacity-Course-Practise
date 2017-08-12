77ce0d7460be6fa495ceb247e021834fbf03324f
victor-iyiola <javafolabi@gmail.com>
2017-08-12 10:59:18 +0100

    Price & Quantity display funcitons written

--- app/src/main/java/com/victoriyiola/udacitypractise/QuantityAndOrder.java
+++ app/src/main/java/com/victoriyiola/udacitypractise/QuantityAndOrder.java
@@ -4,10 +4,14 @@
 
 package com.victoriyiola.udacitypractise;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class QuantityAndOrder extends AppCompatActivity {
 
@@ -17,12 +21,37 @@ public class QuantityAndOrder extends AppCompatActivity {
     setContentView(R.layout.activity_quantity_and_order);
   }
 
+  /**
+   * Fires when the "order" button is clicked.
+   *
+   * @param view View
+   */
   public void submitOrder(View view) {
-    display(1);
+    displayPrice(77 * 2 + 1);
+    displayQuantity(2 * 5);
   }
 
-  public void display(int number) {
+  /**
+   * Displays the calculated quantity into the quantity text view.
+   *
+   * @param quantity int
+   */
+  public void displayQuantity(int quantity) {
+    TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+    quantityTextView.setText(String.valueOf(quantity));
+  }
 
+  /**
+   * Displays the calculated price into the price text view.
+   *
+   * @param price int
+   */
+  public void displayPrice(int price) {
+    TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+      priceTextView.setText(NumberFormat.getCurrencyInstance().format(price));
+    else
+      priceTextView.setText(String.format("$%s", String.valueOf(price)));
   }
 
 }
