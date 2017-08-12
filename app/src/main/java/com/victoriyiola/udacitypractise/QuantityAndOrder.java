56aa13dc6900c9f40335a023dea1c3cf38fadc98
victor-iyiola <javafolabi@gmail.com>
2017-08-12 09:33:14 +0100

    Quantity and Order Project

---
+++ app/src/main/java/com/victoriyiola/udacitypractise/QuantityAndOrder.java
@@ -0,0 +1,21 @@
+package com.victoriyiola.udacitypractise;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/*
+ * @author "Victor I. Afolabi"
+ * Created by victor
+ * A.I. engineer & Software developer
+ * javafolabi@gmail.com
+ * On 12, Aug 2017 @ 9:26 AM
+ * Copyright victor © 2017. All rights reserved.
+ */
+public class QuantityAndOrder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
