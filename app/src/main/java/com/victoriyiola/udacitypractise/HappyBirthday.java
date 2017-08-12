ce6b64d840064ae0a01c6cfa51e94abb9b0dd2f9
victor-iyiola <javafolabi@gmail.com>
2017-08-12 07:54:20 +0100

    Happy birthday challenge

---
+++ app/src/main/java/com/victoriyiola/udacitypractise/HappyBirthday.java
@@ -0,0 +1,22 @@
+package com.victoriyiola.udacitypractise;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/*
+ * @author "Victor I. Afolabi"
+ * Created by victor
+ * A.I. engineer & Software developer
+ * javafolabi@gmail.com
+ * On 12, Aug 2017 @ 7:50 AM
+ * Copyright victor © 2017. All rights reserved.
+ */
+public class HappyBirthday extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_happy_birthday);
+    }
+
+}
