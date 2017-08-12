07cc8508a7bc39c200f65cad3b556c492df75e0a
victor-iyiola <javafolabi@gmail.com>
2017-08-12 09:35:19 +0100

    Quantity and Order Project

--- app/src/main/java/com/victoriyiola/udacitypractise/QuantityAndOrder.java
+++ app/src/main/java/com/victoriyiola/udacitypractise/QuantityAndOrder.java
@@ -17,5 +17,6 @@ public class QuantityAndOrder extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quantity_and_order);
     }
 }
